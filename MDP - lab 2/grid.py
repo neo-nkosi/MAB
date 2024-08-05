dd4fb28e9283a6302b1e76ac76a3241404bfc4cf
Joshuaj0209 <127685979+Joshuaj0209@users.noreply.github.com>
2024-08-05 12:21:39 +0200

    Execute action function implemented to move agent's position

--- MDP - lab 2/grid.py
+++ MDP - lab 2/grid.py
@@ -2,7 +2,7 @@
 rows = 7
 cols = 7
 # Created a grid of pairs. 1st element is the type of block (available, obstacle, goal).
-# 2nd element is the optimal value for that block
+# 2nd element is the reward of that block
 grid = [[('.',-1) for _ in range(cols)] for _ in range(rows)]
 
 # goal position
@@ -21,3 +21,31 @@ for i in range(rows):
     for j in range(cols):
         print(grid[i][j][0],end=" ")
     print()
+
+actions = {
+    'N': (-1,0),
+    'E': (0,1),
+    'S': (1,0),
+    'W': (0,-1)
+}
+
+def execute_action(action, state):
+    x, y = state
+    dx, dy = action
+    new_state = (x+dx, y+dy)
+
+    # check if the action does not move the agent out of bounds or into an obstacle
+    if (0<=new_state[0]<rows and 0<=new_state[1]<cols) and (grid[new_state[0]][new_state[1]][0] != 'X'):
+        return new_state, grid[new_state[0]][new_state[1]][1]
+    else:
+        # agent moves out of bounds or into an obstacle, keep agent at same position with -1 penalty
+        return state, -1
+    
+
+current_state = initial_position
+action = input("Please enter move:")
+
+new_state, reward = execute_action(actions[action], current_state)
+
+print("new state:", new_state)
+print("reward:", reward)
