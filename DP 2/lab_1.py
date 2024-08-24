73e9ecad036ccdb549ed9cdbf7c28ce814a43c83
naomi muzamani <naomineo4@gmail.com>
2024-08-24 12:17:54 +0200

    policy evaluation and iteration

--- DP 2/lab_1.py
+++ DP 2/lab_1.py
@@ -45,41 +45,43 @@ def print_trajectory_grid(trajectory, shape):
 def policy_evaluation(env, policy, discount_factor=1.0, theta=0.00001):
     """
     Evaluate a policy given an environment and a full description of the environment's dynamics.
-
-    Args:
-
-        env: OpenAI environment.
-            env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.observation_space.n is a number of states in the environment.
-            env.action_space.n is a number of actions in the environment.
-        policy: [S, A] shaped matrix representing the policy.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-        discount_factor: Gamma discount factor.
-
-    Returns:
-        Vector of length env.observation_space.n representing the value function.
     """
+    V = np.zeros(env.observation_space.n)
+    while True:
+        delta = 0
+        for s in range(env.observation_space.n):
+            v = 0
+            for a, action_prob in enumerate(policy[s]):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    v += action_prob * prob * (reward + discount_factor * V[next_state])
+            delta = max(delta, np.abs(v - V[s]))
+            V[s] = v
+        if delta < theta:
+            break
+    return V
 
 
 
 def policy_iteration(env, policy_evaluation_fn=policy_evaluation, discount_factor=1.0):
     """
     Iteratively evaluates and improves a policy until an optimal policy is found.
-
-    Args:
-        env: The OpenAI environment.
-        policy_evaluation_fn: Policy Evaluation function that takes 3 arguments:
-            env, policy, discount_factor.
-        discount_factor: gamma discount factor.
-
-    Returns:
-        A tuple (policy, V).
-        policy is the optimal policy, a matrix of shape [S, A] where each state s
-        contains a valid probability distribution over actions.
-        V is the value function for the optimal policy.
-
     """
+    policy = np.ones([env.observation_space.n, env.action_space.n]) / env.action_space.n
+    while True:
+        V = policy_evaluation_fn(env, policy, discount_factor)
+        policy_stable = True
+        for s in range(env.observation_space.n):
+            chosen_action = np.argmax(policy[s])
+            action_values = np.zeros(env.action_space.n)
+            for a in range(env.action_space.n):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    action_values[a] += prob * (reward + discount_factor * V[next_state])
+            best_action = np.argmax(action_values)
+            if chosen_action != best_action:
+                policy_stable = False
+            policy[s] = np.eye(env.action_space.n)[best_action]
+        if policy_stable:
+            return policy, V
 
     def one_step_lookahead(state, V):
         """
@@ -156,17 +158,15 @@ def main():
     # TODO: generate random policy
     num_actions = env.action_space.n
     num_states = env.observation_space.n
-
     random_policy = np.ones([num_states, num_actions]) / num_actions
 
     print("*" * 5 + " Policy evaluation " + "*" * 5)
     print("")
 
-    # TODO: evaluate random policy
     v = policy_evaluation(env, random_policy)
 
-    # TODO: print state value for each state, as grid shape
-
+    print("State values (Policy Evaluation):")
+    print(v.reshape(env.shape))
 
     # Test: Make sure the evaluated policy is what we expected
     expected_v = np.array([-106.81, -104.81, -101.37, -97.62, -95.07,
@@ -178,12 +178,16 @@ def main():
 
     print("*" * 5 + " Policy iteration " + "*" * 5)
     print("")
-    # TODO: use  policy improvement to compute optimal policy and state values
-    policy, v = [], []  # call policy_iteration
 
-    # TODO Print out best action for each state in grid shape
+    policy, v = policy_iteration(env)
 
-    # TODO: print state value for each state, as grid shape
+    # Print out best action for each state in grid shape
+    print("Optimal Policy (Policy Iteration):")
+    optimal_policy_grid = np.array([np.argmax(policy[s]) for s in range(env.observation_space.n)])
+    print(optimal_policy_grid.reshape(env.shape))
+
+    print("State values (Policy Iteration):")
+    print(v.reshape(env.shape))
 
     # Test: Make sure the value function is what we expected
     expected_v = np.array([-8., -7., -6., -5., -4.,
